1734e10162b9f607e403b04013001de25b3f40d6
Alex Havkin <fre3run@gmail.com>
2020-09-07 20:39:05 +0300

    installed UI Kitten styles and axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
 
 import Navigator from './navigation';
 import store from './redux/store';
@@ -10,9 +12,11 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <Provider store={store}>
-        <SafeAreaView style={s.container}>
-          <Navigator />
-        </SafeAreaView>
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <SafeAreaView style={s.container}>
+            <Navigator />
+          </SafeAreaView>
+        </ApplicationProvider>
       </Provider>
     </>
   );
@@ -22,5 +26,5 @@ export default App;
 
 const s = StyleSheet.create({
   container: { flex: 1 }
-})
+});
 
