53b9b9c82189731e1e8239a4870930aa763e40f6
Alex Havkin <fre3run@gmail.com>
2020-09-07 17:45:01 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { Provider } from 'react-redux'
 
 import Navigator from './navigation';
+import store from './redux/store';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={s.container}>
-        <Navigator />
-      </SafeAreaView>
+      <Provider store={store}>
+        <SafeAreaView style={s.container}>
+          <Navigator />
+        </SafeAreaView>
+      </Provider>
     </>
   );
 };
