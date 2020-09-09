21fc20f86e5c903353e11b7b7ae16b70fbdbf58a
Alex Havkin <fre3run@gmail.com>
2020-09-09 15:31:06 +0300

    connected redux persist

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import Navigator from './navigation';
-import store from './store';
+import Redux from './store';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <Provider store={store}>
-        <ApplicationProvider {...eva} theme={eva.light}>
-          <SafeAreaView style={s.container}>
-            <Navigator />
-          </SafeAreaView>
-        </ApplicationProvider>
+      <Provider store={Redux.store}>
+        <PersistGate loading={null} persistor={Redux.persistor}>
+          <ApplicationProvider {...eva} theme={eva.light}>
+            <SafeAreaView style={s.container}>
+              <Navigator />
+            </SafeAreaView>
+          </ApplicationProvider>
+        </PersistGate>
       </Provider>
     </>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
 import thunk from 'redux-thunk';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import reducers from './reducers';
 
-export default createStore(reducers, applyMiddleware(thunk));
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: ['favorites']
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = createStore(persistedReducer, applyMiddleware(thunk))
+const persistor = persistStore(store)
+export default { store, persistor }
